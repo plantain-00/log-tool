f3789d3096427e2f958fd25bc080bb71c4eb92c6
york yao <yaoao12306@outlook.com>
2017-06-30 22:24:13 +0800

    lint config js file

--- static/rev-static.config.js
+++ static/rev-static.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-    inputFiles: [
-        "static/vendor.bundle.css",
-        "static/index.bundle.css",
-        "static/vendor.bundle.js",
-        "static/index.bundle.js",
-        "static/index.ejs.html"
-    ],
-    outputFiles: file => file.replace(".ejs", ""),
-    json: false,
-    ejsOptions: {
-        rmWhitespace: true
-    },
-    sha: 256,
-    customNewFileName: (filePath, fileString, md5String, baseName, extensionName) => baseName + "-" + md5String + extensionName,
-    noOutputFiles: [],
+  inputFiles: [
+    'static/vendor.bundle.css',
+    'static/index.bundle.css',
+    'static/vendor.bundle.js',
+    'static/index.bundle.js',
+    'static/index.ejs.html'
+  ],
+  outputFiles: file => file.replace('.ejs', ''),
+  json: false,
+  ejsOptions: {
+    rmWhitespace: true
+  },
+  sha: 256,
+  customNewFileName: (filePath, fileString, md5String, baseName, extensionName) => baseName + '-' + md5String + extensionName,
+  noOutputFiles: []
 }
--- static/webpack.config.js
+++ static/webpack.config.js
@@ -1,39 +1,38 @@
-const webpack = require("webpack");
-const path = require("path");
+const webpack = require('webpack')
 
 module.exports = {
-    entry: {
-        index: "./static/index",
-        vendor: "./static/vendor"
-    },
-    output: {
-        path: __dirname,
-        filename: "[name].bundle.js"
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            "process.env": {
-                "NODE_ENV": JSON.stringify("production")
-            }
-        }),
-        new webpack.NoEmitOnErrorsPlugin(),
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false,
-            },
-            output: {
-                comments: false,
-            },
-        }),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: ["index", "vendor"]
-        }),
-    ],
-    resolve: {
-        alias: {
-            "vue$": "vue/dist/vue.min.js",
-            "moment": "moment/min/moment.min.js",
-            "protobufjs": "protobufjs/dist/protobuf.min.js"
-        }
+  entry: {
+    index: './static/index',
+    vendor: './static/vendor'
+  },
+  output: {
+    path: __dirname,
+    filename: '[name].bundle.js'
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      output: {
+        comments: false
+      }
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: ['index', 'vendor']
+    })
+  ],
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.min.js',
+      'moment': 'moment/min/moment.min.js',
+      'protobufjs': 'protobufjs/dist/protobuf.min.js'
     }
-};
+  }
+}
