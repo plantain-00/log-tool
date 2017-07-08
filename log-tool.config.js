51595e0a67c1a00b3fe10f0526276d74000a0eaf
york yao <yaoao12306@outlook.com>
2017-07-09 07:09:48 +0800

    fix scripts

--- log-tool.config.js
+++ log-tool.config.js
@@ -1,44 +1,44 @@
 module.exports = {
-    /**
-     * transport logs from another log-tool server.
-     */
+  /**
+   * transport logs from another log-tool server.
+   */
   inflow: {
     enabled: true,
     port: 8001,
     host: 'localhost',
-        /**
-         * with this path, you can post logs to http://localhost:8001/logs
-         */
+    /**
+     * with this path, you can post logs to http://localhost:8001/logs
+     */
     httpFallbackPath: '/logs'
   },
-    /**
-     * transport logs to another log-tool server.
-     */
+  /**
+   * transport logs to another log-tool server.
+   */
   outflow: {
     enabled: false,
     url: 'ws://localhost:8001'
   },
-    /**
-     * watch log directories or files and read logs.
-     */
+  /**
+   * watch log directories or files and read logs.
+   */
   watcher: {
     enabled: true,
-        /**
-         * paths of directories or files to be watched.
-         */
+    /**
+     * paths of directories or files to be watched.
+     */
     paths: [
       './logs/'
     ],
-        /**
-         * path of the file that stores the status of the watched files.
-         */
+    /**
+     * path of the file that stores the status of the watched files.
+     */
     filePositionsDataPath: './log-tool.watcher.data',
-        /**
-         * parse a line of log string to get time and other valid information.
-         * line: string, the log line,
-         * moment: Object, the object from moment.js
-         * filepath: string, the path of the log file
-         */
+    /**
+     * parse a line of log string to get time and other valid information.
+     * line: string, the log line,
+     * moment: Object, the object from moment.js
+     * filepath: string, the path of the log file
+     */
     parseLine: (line, moment, filepath) => {
       return {
         skip: false, // if true, just skip this line of log
@@ -47,52 +47,52 @@ module.exports = {
       }
     }
   },
-    /**
-     * push new logs to a web page, for monitor purpose.
-     */
+  /**
+   * push new logs to a web page, for monitor purpose.
+   */
   gui: {
     enabled: true,
     port: 9000,
     host: 'localhost'
   },
-    /**
-     * transport logs to elastic search server for searching old logs purpose.
-     */
+  /**
+   * transport logs to elastic search server for searching old logs purpose.
+   */
   elastic: {
     enabled: false,
         // `tool` is the index name, `logs` is the type name, they are all needed.
     url: 'http://localhost:9200/tool/logs'
   },
-    /**
-     * transport data by protobuf binary, rather than json string.
-     */
+  /**
+   * transport data by protobuf binary, rather than json string.
+   */
   protobuf: {
     enabled: true
   },
-    /**
-     * watch the size of folder.
-     */
+  /**
+   * watch the size of folder.
+   */
   folderSizeWatcher: {
     enabled: true,
     folders: {
       mysqlSize: './static/'
     }
   },
-    /**
-     * count the logs.
-     */
+  /**
+   * count the logs.
+   */
   countLogs: {
     enabled: true
   },
-    /**
-     * show os information.
-     */
+  /**
+   * show os information.
+   */
   os: {
     enabled: true
   },
   sqlite: {
     filePath: './data.db',
-        // if enabled, will save samples to sqlite.
+    // if enabled, will save samples to sqlite.
     samples: true
   }
 }
