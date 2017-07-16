14ccd631cf3e189b77e7e6ef8f4a7218031028eb
york yao <yaoao12306@outlook.com>
2017-07-16 09:26:58 +0800

    add missing config

---
+++ log-tool.config.js
@@ -0,0 +1,6 @@
+module.exports = function (defaultConfig) {
+  defaultConfig.elastic.enabled = false
+  defaultConfig.folderSizeWatcher.folders.mysqlSize = './static/'
+  defaultConfig.gui.port = 9000
+  defaultConfig.watcher.paths = ['./logs/']
+}
