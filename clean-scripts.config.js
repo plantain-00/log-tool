8452f8128ef3ec47e8d0bdb6ffd4fd029192713f
york yao <yaoao12306@outlook.com>
2017-08-07 23:17:23 +0800

    update webpack config; improve scripts

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -1,33 +1,46 @@
 module.exports = {
   build: [
-    `rimraf dist/`,
-    `rimraf static/*.bundle-*.js static/vendor.bundle-*.css static/index.bundle-*.css`,
     `types-as-schema src/types.ts --json static/ --protobuf static/protocol.proto`,
-    `file2variable-cli static/*.template.html static/protocol.proto static/*-protocol.json -o static/variables.ts --html-minify --json --protobuf --base static`,
-    `file2variable-cli src/sql/*.sql -o src/variables.ts --base src/sql`,
-    `tsc -p src`,
-    `tsc -p static`,
-    `webpack --display-modules --config static/webpack.config.js`,
-    `lessc ./static/index.less > ./static/index.css`,
-    `cleancss ./node_modules/github-fork-ribbon-css/gh-fork-ribbon.css ./node_modules/tab-container-component/tab-container.min.css -o ./static/vendor.bundle.css`,
-    `cleancss ./static/index.css -o ./static/index.bundle.css`,
+    {
+      back: [
+        `file2variable-cli src/sql/*.sql -o src/variables.ts --base src/sql`,
+        `rimraf dist/`,
+        `tsc -p src`
+      ],
+      front: {
+        js: [
+          `file2variable-cli static/*.template.html static/protocol.proto static/*-protocol.json -o static/variables.ts --html-minify --json --protobuf --base static`,
+          `tsc -p static`,
+          `webpack --display-modules --config static/webpack.config.js`
+        ],
+        css: [
+          `lessc ./static/index.less > ./static/index.css`,
+          `cleancss ./node_modules/github-fork-ribbon-css/gh-fork-ribbon.css ./node_modules/tab-container-component/tab-container.min.css -o ./static/vendor.bundle.css`,
+          `cleancss ./static/index.css -o ./static/index.bundle.css`
+        ],
+        clean: `rimraf static/*.bundle-*.js static/vendor.bundle-*.css static/index.bundle-*.css`
+      }
+    },
     `rev-static --config static/rev-static.config.js`
   ],
-  lint: [
-    `tslint "src/*.ts" "static/*.ts"`,
-    `standard "**/*.config.js"`,
-    `stylelint "online/**/*.less"`
-  ],
-  test: [
-    'tsc -p spec',
-    'jasmine',
-    'tsc -p static_spec',
-    'karma start static_spec/karma.config.js'
-  ],
-  fix: [
-    `standard --fix "**/*.config.js"`
-  ],
-  release: [
-    `clean-release`
-  ]
+  lint: {
+    ts: `tslint "src/*.ts" "static/*.ts"`,
+    js: `standard "**/*.config.js"`,
+    less: `stylelint "online/**/*.less"`
+  },
+  test: {
+    jasmine: [
+      'tsc -p spec',
+      'jasmine'
+    ],
+    karma: [
+      'tsc -p static_spec',
+      'karma start static_spec/karma.config.js'
+    ]
+  },
+  fix: {
+    js: `standard --fix "**/*.config.js"`,
+    less: `stylelint --fix "online/**/*.less"`
+  },
+  release: `clean-release`
 }
