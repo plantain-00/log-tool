57e2a8660bb308e1baeec793b84a30a9138c12d8
york yao <yaoao12306@outlook.com>
2018-03-04 19:45:08 +0800

    chore: update dependencie

--- static/webpack.config.js
+++ static/webpack.config.js
@@ -1,36 +1,26 @@
-const webpack = require('webpack')
-
 module.exports = {
   entry: {
-    index: './static/index',
-    vendor: './static/vendor'
+    index: './static/index'
   },
   output: {
     path: __dirname,
     filename: '[name].bundle.js'
   },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    }),
-    new webpack.NoEmitOnErrorsPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      output: {
-        comments: false
-      },
-      exclude: [
-      ]
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: ['index', 'vendor']
-    })
-  ],
   resolve: {
     alias: {
       'moment': 'moment/min/moment.min.js',
       'protobufjs': 'protobufjs/dist/protobuf.min.js'
+    }
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor',
+          chunks: 'all'
+        }
+      }
     }
   }
 }
