c206d7b3abcd2e4ffeb32c3f4501e32dae5ea8b2
york yao <yaoao12306@outlook.com>
2018-11-03 08:57:11 +0800

    chore: update dependencies

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -51,7 +51,7 @@ module.exports = {
     export: `no-unused-export ${tsFiles} ${lessFiles}`,
     commit: `commitlint --from=HEAD~1`,
     markdown: `markdownlint README.md`,
-    typeCoverage: 'type-coverage -p src --at-least 98',
+    typeCoverage: 'type-coverage -p src --at-least 97',
     typeCoverageStatic: 'type-coverage -p static --at-least 89'
   },
   test: {
